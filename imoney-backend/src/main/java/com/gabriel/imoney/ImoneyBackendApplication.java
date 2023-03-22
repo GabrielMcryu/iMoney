06b857524e8c0355d89ac6d68a1a76bf0bcef391
GabrielMcryu <gabrielmcryu@gmail.com>
2023-03-22 15:52:19 +0300

    First commit

---
+++ imoney-backend/src/main/java/com/gabriel/imoney/ImoneyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.imoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoneyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoneyBackendApplication.class, args);
+	}
+
+}
---
+++ imoney-backend/src/test/java/com/gabriel/imoney/ImoneyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.imoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoneyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
